974e4f9ce9e115a4935521ef3a1d431f76650512
bhargavi <bhargavi@umich.edu>
2020-03-31 23:25:14 -0400

    Updated app.js and index.html

---
+++ tasks/static/tasks/app.js
@@ -0,0 +1,52 @@
+const addForm=document.querySelector('.add1');
+const list=document.querySelector('.todos1');
+const search=document.querySelector('.search1 input');
+
+const generateTemplate = function(todo){
+    const html = 
+        `<li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>`
+    list.innerHTML+= html;
+};
+
+addForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const todo=addForm.add.value.trim();
+    //trim removes any whitespace before or after the string
+    if(todo.length>0){
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
+list.addEventListener('click',e=>{
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos=function(term){
+    Array.from(list.children)
+    .filter((todo)=>{
+        return !todo.textContent.includes(term)
+    }).forEach((todo)=>{
+        todo.classList.add('filtered')
+    });
+
+    Array.from(list.children)
+    .filter((todo) => {
+        return todo.textContent.includes(term)
+    }).forEach((todo) => {
+        todo.classList.remove('filtered');
+    });
+};
+
+search.addEventListener('keyup',()=>{
+    const term=search.value.trim();
+    filterTodos(term);
+});
+
+
+
