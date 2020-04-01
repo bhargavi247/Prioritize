5d50fddf8ff6102b54fb7358b58c89b6bc601f9b
bhargavi <bhargavi@umich.edu>
2020-04-01 10:41:32 -0400

    Added urgent and important to html, made some changes to css, updated app.js

--- tasks/static/tasks/app.js
+++ tasks/static/tasks/app.js
@@ -5,8 +5,10 @@ const search=document.querySelector('.search1 input');
 const generateTemplate = function(todo){
     const html = 
         `<li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${todo}</span>
-            <i class="far fa-trash-alt delete"></i>
+            <span class="col-sm-9 text-left">${todo}</span>
+            <i class="fas fa-exclamation-triangle urgent col-sm-1"></i>
+            <i class="fas fa-star important col-sm-1"></i>
+            <i class="far fa-trash-alt delete col-sm-1"></i>
         </li>`
     list.innerHTML+= html;
 };
